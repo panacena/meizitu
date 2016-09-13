3420d8beedad285d742ff5ae0409ca17fa45720f
panacena <394804550@qq.com>
2016-09-13 16:09:31 +0800

    第一次提交

--- app/src/main/java/com/zkk/meizitu/MainActivity.java
+++ app/src/main/java/com/zkk/meizitu/MainActivity.java
@@ -2,12 +2,16 @@ package com.zkk.meizitu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        Log.d("zkk---","--");
         setContentView(R.layout.activity_main);
     }
 }
