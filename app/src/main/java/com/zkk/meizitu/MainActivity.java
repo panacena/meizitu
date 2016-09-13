13976b35b0b6b218c5ba9f532ab38c190a5db671
panacena <394804550@qq.com>
2016-09-13 19:39:48 +0800

    home

--- app/src/main/java/com/zkk/meizitu/MainActivity.java
+++ app/src/main/java/com/zkk/meizitu/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Log.d("zkk---","--");
+
         setContentView(R.layout.activity_main);
     }
 }
