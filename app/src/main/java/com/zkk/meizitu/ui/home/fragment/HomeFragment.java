99c9a74751ffcfce884a667f6480d7e1c788c0c8
panacena <394804550@qq.com>
2016-09-14 17:46:52 +0800

    增加滑动切换和开始视频

--- app/src/main/java/com/zkk/meizitu/ui/home/fragment/HomeFragment.java
+++ app/src/main/java/com/zkk/meizitu/ui/home/fragment/HomeFragment.java
@@ -3,13 +3,21 @@ package com.zkk.meizitu.ui.home.fragment;
 import android.view.View;
 
 import com.github.obsessive.library.eventbus.EventCenter;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
 import com.zkk.meizitu.R;
 import com.zkk.meizitu.ui.base.BaseFragment;
+
+import butterknife.InjectView;
 
 /**
  * Created by Administrator on 2016/9/13 0013.
  */
 public class HomeFragment extends BaseFragment {
+
+
+    @InjectView(R.id.banner)
+    Banner banner;
     @Override
     protected void onFirstUserVisible() {
 
@@ -32,7 +40,10 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initViewsAndEvents() {
-
+        String[] images= new String[] {"url"};
+        String[] titles=new String[]{"标题"};
+        banner.setBannerStyle(BannerConfig.NUM_INDICATOR_TITLE);
+        banner.setImages(images);
     }
 
     @Override
